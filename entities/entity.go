0b5d171f2994c2f29f50eb38b1505a7eb33e7d11
moronimotta <dareevil7@gmail.com>
2025-05-19 13:46:51 -0300

    gateway necessary db tables

--- entities/entity.go
+++ entities/entity.go
@@ -2,22 +2,28 @@ package entities
 
 import "finances-api/utils/meta"
 
-// Product
-// Invoice
-// Customer (save in the user-api service)
-
-type Product struct {
+type Products struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Price       int64  `json:"price"`
+	ExternalID  string `json:"external_id"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
 	DeletedAt   string `json:"deleted_at"`
-	ExternalID  string `json:"external_id"`
 }
 
-type Invoice struct {
+type User_Products struct {
+	ID        string `json:"id"`
+	UserID    string `json:"user_id"`
+	ProductID string `json:"product_id"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	DeletedAt string `json:"deleted_at"`
+	Status    string `json:"status"`
+}
+
+type Invoices struct {
 	ID            string    `json:"id"`
 	CustomerID    string    `json:"customer_id"`
 	Amount        int64     `json:"amount"`
@@ -27,5 +33,26 @@ type Invoice struct {
 	ExternalID    string    `json:"external_id"`
 	PaymentStatus string    `json:"payment_status"`
 	PaymentMethod string    `json:"payment_method"`
+	Currency      string    `json:"currency"`
 	Meta          meta.Meta `json:"meta"`
+}
+
+type Transactions struct {
+	ID          string `json:"id"`
+	AmountPayed int64  `json:"amount_paid"`
+	InvoiceID   string `json:"invoice_id"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+	DeletedAt   string `json:"deleted_at"`
+	ExternalID  string `json:"external_id"`
+}
+
+type Gateway struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	SecretKey    string `json:"secret_key"`
+	ClientSecret string `json:"client_secret"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
+	DeletedAt    string `json:"deleted_at"`
 }
